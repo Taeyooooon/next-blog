3a44ed161fc9029900462cc6f34176c038f080ae
Taeyooooon <eric568@naver.com>
2023-03-22 16:31:35 +0900

    Add: markdown viewer syntax highlighter

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import MarkdownViewer from '@/app/components/MarkdownViewer';
 import { getPostData } from '@/service/posts';
+import Image from 'next/image';
+import { AiTwotoneCalendar } from 'react-icons/ai';
 
 type Props = {
   params: {
@@ -8,12 +10,27 @@ type Props = {
 };
 
 export default async function PostPage({ params: { slug } }: Props) {
-  const posts = await getPostData(slug);
+  const { title, description, date, path, content } = await getPostData(slug);
 
   return (
-    <>
-      <h1>{posts.title}</h1>
-      <MarkdownViewer content={posts.content} />
-    </>
+    <article className=' rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4 '>
+      <Image
+        className=' w-full max-h-[500px]'
+        src={`/images/posts/${path}.png `}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section className='flex flex-col p-4'>
+        <div className='flex items-center self-end text-sky-600'>
+          <AiTwotoneCalendar />
+          <p className=' font-semibold ml-2'>{date.toString()}</p>
+        </div>
+        <h1 className='text-4xl font-bold'>{title}</h1>
+        <p className=' text-xl font-bold'>{description}</p>
+        <div className=' w-48 border-2 border-sky-600 mt-4 mb-8' />
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 }
