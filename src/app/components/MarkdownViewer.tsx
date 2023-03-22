4504be7b816fcca943c26c7d19ad21482c15f2df
Taeyooooon <eric568@naver.com>
2023-03-22 17:09:27 +0900

    Add: 전 후 포스트 이동 배너

--- src/app/components/MarkdownViewer.tsx
+++ src/app/components/MarkdownViewer.tsx
@@ -2,7 +2,7 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import Image from 'next/image';
 
 export default function MarkdownViewer({ content }: { content: string }) {
@@ -18,7 +18,7 @@ export default function MarkdownViewer({ content }: { content: string }) {
               language={match[1]}
               PreTag='div'
               {...props}
-              style={materialDark}
+              style={dracula}
             >
               {String(children).replace(/\n$/, '')}
             </SyntaxHighlighter>
