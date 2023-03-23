0ed6fce00597449eff348f1277f6d335b1e59a26
Taeyooooon <eric568@naver.com>
2023-03-23 17:44:15 +0900

    Add: contact layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,26 @@
+import Hero from '../components/Hero';
+
+const TITLE_CLASS = 'text-2xl font-bold text-gray-800 my-2';
+
 export default function AboutPage() {
-  return <h1>about 페이지</h1>;
+  return (
+    <>
+      <Hero />
+      <section className=' bg-gray-100 shadow-lg p-8 m-8 text-center'>
+        <h2 className={TITLE_CLASS}>How Am I?</h2>
+        <p>프론트엔드 개발자</p>
+        <h2 className={TITLE_CLASS}>Career</h2>
+        <p>
+          취준
+        </p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>
+          React, Next.js <br />
+          Scss, Styled-Components, Tailwind <br />
+          Git, Github, Github-Actions <br />
+          VSCode
+        </p>
+      </section>
+    </>
+  );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,37 @@
+import { AiFillGithub } from 'react-icons/ai';
+import { SiVelog } from 'react-icons/si';
+import ContactForm from '../components/ContactForm';
+const LINKS = [
+  {
+    icon: <AiFillGithub />,
+    url: 'https://github.com/Taeyooooon',
+  },
+  {
+    icon: <SiVelog />,
+    url: 'https://velog.io/@taeyooooon',
+  },
+];
+
 export default function ContactPage() {
-  return <h1>컨텍 페이지</h1>;
+  return (
+    <section className=' flex flex-col items-center'>
+      <h2 className='text-3xl font-bold my-2'>Contact Me</h2>
+      <p>eric8401@gmail.com</p>
+      <ul className='flex gap-4 my-2'>
+        {LINKS.map((link, index) => (
+          <a
+            key={index}
+            href='link.url'
+            target='_blank'
+            rel='noreferrer'
+            className='text-5xl hover:text-yellow-500  '
+          >
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className=' text-3xl font-bold my-8'>Or Send an Email</h2>
+      <ContactForm />
+    </section>
+  );
 }
