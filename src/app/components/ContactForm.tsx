3d506cadf6d1c9e77d719dd23aa8583d377a2e49
Taeyooooon <eric568@naver.com>
2023-03-23 18:50:42 +0900

    Add: contact 이메일 전송 로직

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -2,6 +2,7 @@
 
 import { ChangeEvent, FormEvent, useState } from 'react';
 import Banner, { BannerData } from './Banner';
+import { sendContactEmail } from '@/service/contact';
 
 type Form = {
   from: string;
@@ -9,12 +10,14 @@ type Form = {
   message: string;
 };
 
+const DEFAULT_DATA = {
+  from: '',
+  subject: '',
+  message: '',
+};
+
 export default function ContactForm() {
-  const [form, setForm] = useState<Form>({
-    from: '',
-    subject: '',
-    message: '',
-  });
+  const [form, setForm] = useState<Form>(DEFAULT_DATA);
   const [banner, setBanner] = useState<BannerData | null>();
 
   const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -24,11 +27,25 @@ export default function ContactForm() {
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(form);
-    setBanner({ message: '성공', state: 'success' });
-    setTimeout(() => {
-      setBanner(null);
-    }, 3000);
+    sendContactEmail(form)
+      .then(() => {
+        setBanner({
+          message: '메일을 성공적으로 보냈습니다.',
+          state: 'success',
+        });
+        setForm(DEFAULT_DATA);
+      })
+      .catch(() => {
+        setBanner({
+          message: '메일전송에 실패했습니다. 다시 시도해 주세요.',
+          state: 'error',
+        });
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setBanner(null);
+        }, 3000);
+      });
   };
 
   return (
@@ -49,6 +66,7 @@ export default function ContactForm() {
           autoFocus
           value={form.from}
           onChange={onChange}
+          className='text-black'
         />
         <label htmlFor='subject' className='font-semibold'>
           Subject
@@ -58,6 +76,7 @@ export default function ContactForm() {
           id='subject'
           name='subject'
           required
+          autoComplete='off'
           value={form.subject}
           onChange={onChange}
           className='text-black'
