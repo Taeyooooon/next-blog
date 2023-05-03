3b46ce97c79d3938f1ea510618637b0ed126f6cc
Taeyooooon <eric568@naver.com>
2023-05-03 15:26:05 +0900

    Refactor: category color case

--- src/app/components/PostCard.tsx
+++ src/app/components/PostCard.tsx
@@ -6,16 +6,38 @@ type Props = {
   post: Post;
 };
 
-export default function PostCard({ post: { title, description, date, category, path } }: Props) {
+export default function PostCard({
+  post: { title, description, date, category, path },
+}: Props) {
+  const getCategoryColor = (category: string) => {
+    switch (category) {
+      case 'my story':
+        return 'bg-green-100';
+      case 'frontend':
+        return 'bg-blue-100';
+      case 'backend':
+        return 'bg-yellow-100';
+      case 'javascript':
+        return 'bg-red-100';
+    }
+  };
   return (
     <Link href={`/posts/${path}`}>
       <article className=' rounded-md overflow-hidden shadow-md hover:shadow-xl'>
-        <Image className='w-full' src={`/images/posts/${path}.png`} alt={title} width={300} height={200} />
+        <Image
+          className='w-full'
+          src={`/images/posts/${path}.png`}
+          alt={title}
+          width={300}
+          height={200}
+        />
         <div className='flex flex-col items-center p-4'>
           <time className='self-end text-gray-700'>{date.toString()}</time>
-          <h3 className='text-lg font-bold'>{title}</h3>
+          <h3 className='text-lg truncate font-bold'>{title}</h3>
           <p className='w-full truncate text-center'>{description}</p>
-          <span className='text-sm rounded-lg bg-green-100 px-2 my-2'>{category}</span>
+          <span className={`text-sm rounded-lg ${getCategoryColor(category)} px-2 my-2`}>
+            {category}
+          </span>
         </div>
       </article>
     </Link>
